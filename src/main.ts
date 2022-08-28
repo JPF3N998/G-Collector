06a1edd7db1b029c3ead8ed4ec175b733d96c9a3
Jose Pablo Feng <jpfeng-dev@proton.me>
2022-08-27 22:09:54 -0600

    Added pinia to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
-import '@/style.css'
-import App from '@/App.vue'
+import { createPinia } from 'pinia'
 import router from '@/router'
-
+import App from '@/App.vue'
+import '@/style.css'
 import '@/services/background/background'
 import '@/services/content/content-script'
 
+const pinia = createPinia();
 
 createApp(App)
+  .use(pinia)
   .use(router)
   .mount('#app')
