3ca3d8fcb777cc6cd392cbbf8b7878b82a471452
Jose Pablo Feng <jpfeng-dev@proton.me>
2022-09-21 19:15:30 -0600

    Created TheFooter component
    + Moved counter
    + Moved export button

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,18 +2,30 @@
 import { useSavedItemsStore } from '@/store/savedItemsStore';
 import { useToExportItemsStore } from '@/store/toExportItemsStore';
 import { computed } from 'vue';
+import ItemsToExportCounterVue from '@/components/ItemsToExportCounter.vue';
 
 const savedItemsStore = useSavedItemsStore();
 const toExportItemsStore = useToExportItemsStore();
 
 const areThereElements = computed(() => savedItemsStore.itemsCount > 0);
+const disableExportButton = computed(() => toExportItemsStore.itemsCount <= 0);
 
+function handleClick() {
+  console.log('click');
+}
 </script>
 
 <template>
-    <span v-if="savedItemsStore.itemsCount">
-      {{`${toExportItemsStore.itemsCount}/${savedItemsStore.itemsCount} selected`}}
-    </span>
+  <section class="wrapper">
+    <ItemsToExportCounterVue />
+    <button
+      v-if="areThereElements"
+      :disabled="disableExportButton"
+      @click="handleClick"
+    >
+      Export
+    </button>
+  </section>
 </template>
 
 <style scoped>
