1f894eb56b239195464e645d547be7cd0626162d
Jose Pablo Feng <jpfeng-dev@proton.me>
2022-09-21 19:05:52 -0600

    Added export button, no action yet

--- src/components/ItemsToExportCounter.vue
+++ src/components/ItemsToExportCounter.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
 import { useSavedItemsStore } from '@/store/savedItemsStore';
 import { useToExportItemsStore } from '@/store/toExportItemsStore';
+import { computed } from 'vue';
 
 const savedItemsStore = useSavedItemsStore();
 const toExportItemsStore = useToExportItemsStore();
+
+const areThereElements = computed(() => savedItemsStore.itemsCount > 0);
+
 </script>
 
 <template>
-  <span v-if="savedItemsStore.itemsCount">
-    {{`${toExportItemsStore.itemsCount}/${savedItemsStore.itemsCount} selected`}}
-  </span>
+  <section class="wrapper">
+    <span v-if="savedItemsStore.itemsCount">
+      {{`${toExportItemsStore.itemsCount}/${savedItemsStore.itemsCount} selected`}}
+    </span>
+    <button v-if="areThereElements">Export</button>
+  </section>
 </template>
+
+<style scoped>
+.wrapper {
+  align-items: flex-end;
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
