f230ab5eebb7d08bb9c5ff3e9a854a2b831d96db
Jose Pablo Feng <jpfeng-dev@proton.me>
2022-09-21 22:11:51 -0600

    Implemented save items as CSV

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -10,8 +10,21 @@ const toExportItemsStore = useToExportItemsStore();
 const areThereElements = computed(() => savedItemsStore.itemsCount > 0);
 const disableExportButton = computed(() => toExportItemsStore.itemsCount <= 0);
 
-function handleClick() {
-  console.log(toExportItemsStore.toCsv());
+/**
+ * Get items to export, "blob it", and prompt download as CSV
+ */
+function exportToCSV() {
+  const csvContent = toExportItemsStore.toCsv();
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' })
+  const url = URL.createObjectURL(blob);
+
+  // Create hidden <a>
+  const hiddenAnchor = document.createElement('a');
+  hiddenAnchor.setAttribute('href', url);
+  
+  // @Todo: Use collections name + date + time for CSV name
+  hiddenAnchor.setAttribute('download', 'Favorite pages.csv');
+  hiddenAnchor.click();
 }
 </script>
 
@@ -21,9 +34,9 @@ function handleClick() {
     <button
       v-if="areThereElements"
       :disabled="disableExportButton"
-      @click="handleClick"
+      @click="exportToCSV"
     >
-      Export
+    To .csv
     </button>
   </section>
 </template>
